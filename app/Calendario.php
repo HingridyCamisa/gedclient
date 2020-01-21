50993ad5461456e2f324aeb14360ada364c58dd9
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2020-01-22 01:02:36 +0200

    calendario work

--- app/Calendario.php
+++ app/Calendario.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Calendario extends Model
 {
-    protected $fillable = ['titulo','data_inicio','data_final'];
+
+    protected $guarded =array();
+
+    public $primaryKey = 'id';
+
+    public $timestamps=true;
 }
